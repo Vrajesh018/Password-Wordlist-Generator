8067cb1046ad158af14222b3afeb7cd315c6a0b7
Vrajesh-018 <vrajeshpantawane018@gmail.com>
2026-08-18 16:19:27 +0530

    passList Class Created

---
+++ main.py
@@ -0,0 +1,14 @@
+class passList:
+    def __init__(self):
+        self.wordLst = []
+
+
+    def Welcome(self):
+        print("="*50, "\n")
+        print("\t Password Wordlist Generator!\n")
+        print("="*50, "\n")
+
+
+if __name__ == "__main__":
+    target = passList()
+    target.Welcome()
