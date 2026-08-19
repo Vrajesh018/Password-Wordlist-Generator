5064468a3b87a8e0118778366f531f2db2773b43
Vrajesh-018 <vrajeshpantawane018@gmail.com>
2026-08-19 20:20:53 +0530

    Welcome page Added

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
+import random
+
 class passList:
     def __init__(self):
+        """ Consttuctor for the class to initialize variables & start program."""
         self.wordLst = []
+        self.specialChar = "\"#^*_+-=~`\\|:;',./!@$%&(){}[]<>?"
 
 
     def Welcome(self):
-        print("="*50, "\n")
-        print("\t Password Wordlist Generator!\n")
-        print("="*50, "\n")
+        """ Welcome page of the program which changes with every time."""
+        col = 25
+        row = 6
+        pattern = self.specialChar[random.randint(0,17)]
+        for i in range(row):
+            if i == 3:
+                print(f"{pattern}\t Password Wordlist Generator! \t\t{pattern}")
+            for j in range(col):
+        
+                if i == 0 or i == row-1 or j == 0 or j == col-1:
+                    print(pattern, end=" ")
+                else:
+                    print(" ", end=" ")
+            print()
 
 
 if __name__ == "__main__":
