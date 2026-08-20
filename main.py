f19ca77a92971c119637c8b6df02b1dec9d54b5d
Vrajesh-018 <vrajeshpantawane018@gmail.com>
2026-08-20 20:10:11 +0530

    Added Target_Info Function

--- main.py
+++ main.py
@@ -5,6 +5,12 @@ class passList:
         """ Consttuctor for the class to initialize variables & start program."""
         self.wordLst = []
         self.specialChar = "\"#^*_+-=~`\\|:;',./!@$%&(){}[]<>?"
+        self.infoValue = {
+            1 : "Name only",
+            2 : "Name & DOB",
+            3 : "Name, DOB & Account name"
+        }
+        self.exit = False
 
 
     def Welcome(self):
@@ -22,8 +28,59 @@ class passList:
                 else:
                     print(" ", end=" ")
             print()
+        print("\n How Much information do you have?\n")
+        for i in self.infoValue:
+            print(f"{i}. {self.infoValue[i]}")
+        while True:
+            try:
+                self.Info_Num = int(input("\nEnter Value number : "))
+                if self.Info_Num > 3:
+                    print("Enter valid option number...")
+                    continue
+            except ValueError:
+                print("Enter valid option number...")
+                continue
+            else:
+                break
 
+    def Target_Info(self):
+        def name():
+            while True:
+                user_input = input("Please enter your name : ")
+                if not user_input.isalpha():
+                    print("\n\t Enter Valid Name!\n")
+                else:
+                    return user_input
+        def DOB():
+            while True:
+                user_input = input("please enter your DOB in YYYYMMDD format : ")
+                if not user_input.isdigit():
+                    print("\n\t Enter Valid Date of Birth in YYYYMMDD format!")
+                elif len(user_input) != 8:
+                    print("\n\t Enter Valid Date of Birth in YYYYMMDD format!")
+                    continue
+                else:
+                    return user_input
+        def accName_input():
+            while True:
+                user_input = input("Please enter Account Name :")
+                if user_input.strip():
+                    return user_input
+ 
+        self.targetName = name()
+
+        if self.Info_Num >= 2:
+            self.targetDOB = DOB()
+
+        if self.Info_Num >= 3:
+            self.accName = accName_input()
+          
 
 if __name__ == "__main__":
-    target = passList()
-    target.Welcome()
+    try:
+        while True:
+            target = passList()
+            target.Welcome()
+            target.Target_Info()
+    except KeyboardInterrupt:
+        print("\n\n\t\t Program Interrupted...\n")
